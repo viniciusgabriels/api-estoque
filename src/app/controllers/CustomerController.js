4e682caa78dc0255d0b9ef3505e58d4305a62b1f
Mateus Luciano <teeusdm@gmail.com>
2021-03-23 19:39:13 -0300

    update customer controller

--- src/app/controllers/CustomerController.js
+++ src/app/controllers/CustomerController.js
@@ -4,7 +4,7 @@ class CustomerController {
   async index(req, res) {
     try {
       const data = await Customer.findAll({
-        attributes: ['id', 'name'],
+        attributes: ['id', 'name', 'phone'],
         order: [['id', 'DESC']],
       })
       
@@ -13,10 +13,72 @@ class CustomerController {
       return res.status(error.status || 400).json(error);
     }
   }
-  async show(req, res) {}
-  async store(req, res) {}
-  async update(req, res) {}
-  async delete(req, res) {}
+
+  async show(req, res) {
+    const { id } = req.params;
+
+    try {
+      const data = await Customer.findOne({
+        attributes: ['id', 'name', 'phone'],
+        where: { id },
+      });
+      
+      return res.json(data);
+    } catch (error) {
+      return res.status(error.status || 400).json(error);
+    }
+  }
+
+  async store(req, res) {
+    const { name, phone } = req.body;
+
+    try {
+      const data = await Customer.create({
+        name,
+        phone,
+      })
+
+      return res.status(201).json(data);
+    } catch (error) {
+      return res.status(error.status || 400).json(error);
+    }
+  }
+
+  async update(req, res) {
+    const { id } = req.params;
+    const { name, phone } = req.body;
+    
+    try {
+      const data = await Customer.update(
+        {
+        name,
+        phone,
+      },
+      {
+        where: { id },
+        returning: ['id', 'name', 'phone']
+      }
+      )
+
+      return data[1];
+    } catch (error) {
+      return res.status(error.status || 400).json(error);
+    }
+  }
+
+  async delete(req, res) {
+    const { id } = req.params;
+
+    try {
+      /////////////// Fazer o detroy das ordens
+
+      await Customer.destroy({
+        where: { id },
+      });
+    } catch (error) {
+      return res.status(error.status || 400).json(error);
+    }
+  }
 }
 
 export default new CustomerController();
