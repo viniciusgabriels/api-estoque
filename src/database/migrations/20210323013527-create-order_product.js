417cc84e36eab66dc13440e2bcc216ba4d011d95
Vinícius Gabriel <viniciusgabriel.rs@hotmail.com>
2021-03-23 20:42:46 -0300

    Model OrderProduct

--- src/database/migrations/20210323013527-create-order_product.js
+++ src/database/migrations/20210323013527-create-order_product.js
@@ -1,6 +1,24 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.createTable('order_product', {
+      order_id: {
+        type: Sequelize.DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        references: {
+          model: 'order',
+          key: 'id',
+        },
+      },
+      product_stock_id: {
+        type: Sequelize.DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        references: {
+          model: 'product_stock',
+          key: 'id',
+        },
+      },
       status: {
         type: Sequelize.DataTypes.STRING,
         allowNull: false,
@@ -12,22 +30,6 @@ module.exports = {
       preco_venda: {
         type: Sequelize.DataTypes.FLOAT,
         allowNull: false,
-      },
-      order_id: {
-        type: Sequelize.DataTypes.INTEGER,
-        allowNull: true,
-        references: {
-          model: 'order',
-          key: 'id',
-        },
-      },
-      product_stock_id: {
-        type: Sequelize.DataTypes.INTEGER,
-        allowNull: true,
-        references: {
-          model: 'product_stock',
-          key: 'id',
-        },
       },
       created_at: {
         type: Sequelize.DataTypes.DATE,
