ed55fbf822fc2016bc200e46859f5f66b0e6846a
Mateus Luciano <teeusdm@gmail.com>
2021-03-23 19:09:46 -0300

    create customer model

---
+++ src/app/models/Customer.js
@@ -0,0 +1,47 @@
+import Sequelize, { Model } from 'sequelize';
+
+class Customer extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        id: {
+          type: Sequelize.DataTypes.INTEGER,
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+        },
+        name: {
+          type: Sequelize.DataTypes.STRING(100),
+          allowNull: false,
+        },
+        phone: {
+          type: Sequelize.DataTypes.INTEGER,
+          allowNull: false,
+        },
+        region_id: {
+          type: Sequelize.DataTypes.INTEGER,
+          allowNull: false,
+          references: {
+            model: 'region',
+            key: 'id',
+          },
+        },
+      },
+      {
+        sequelize,
+        tableName: 'customers'
+      }
+    );
+
+    return this;
+  }
+
+  static associate(models) {
+    this.belongsTo(models.Region, {
+      as: 'customer',
+      foreignKey: 'region_id',
+    });
+  }
+}
+
+export default Customer;
