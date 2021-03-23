3a49733c9ebdf18eec4aabfff1af1273177dacb2
Vinícius Gabriel <viniciusgabriel.rs@hotmail.com>
2021-03-23 19:59:24 -0300

    order products

--- src/database/migrations/20210323013527-create-order_product.js
+++ src/database/migrations/20210323013527-create-order_product.js
@@ -1,21 +1,46 @@
-'use strict';
-
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('order_product', {
+      status: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false,
+      },
+      quantity: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false,
+      },
+      preco_venda: {
+        type: Sequelize.DataTypes.FLOAT,
+        allowNull: false,
+      },
+      order_id: {
+        type: Sequelize.DataTypes.INTEGER,
+        allowNull: true,
+        references: {
+          model: 'order',
+          key: 'id',
+        },
+      },
+      product_stock_id: {
+        type: Sequelize.DataTypes.INTEGER,
+        allowNull: true,
+        references: {
+          model: 'product_stock',
+          key: 'id',
+        },
+      },
+      created_at: {
+        type: Sequelize.DataTypes.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DataTypes.DATE,
+        allowNull: false,
+      },
+    });
   },
 
-  down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('order_product');
+  },
 };
