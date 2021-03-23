4157bf1ccbddaabbe1647d09ba9e66bbedfd4b90
RafaelGoetz <rafagoetz@gmail.com>
2021-03-23 20:01:57 -0300

    newcommity

--- src/app/routers/index.js
+++ src/app/routers/index.js
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
 import category from './category';
 import product from './product';
 import swagger from './swagger';
-
-export default [
-  category, product , swagger 
-];
-=======
 import customer from './customer';
 
-export default [customer];
->>>>>>> c38c25a5d5feae899684f93fe1024ecad148df8c
+export default [ category, product , swagger, customer ];
