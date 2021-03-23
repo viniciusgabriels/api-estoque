54038698a90d680d8d1ff8493578cbddf89ef537
Mateus Luciano <teeusdm@gmail.com>
2021-03-23 19:05:12 -0300

    create type orders migration

--- src/database/migrations/20210323012353-create-type.js
+++ src/database/migrations/20210323012353-create-type.js
@@ -1,21 +1,31 @@
-'use strict';
-
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable(
+      'type',
+      {
+        id: {
+          type: Sequelize.DataTypes.INTEGER,
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+        },
+        description: {
+          type: Sequelize.DataTypes.STRING(240),
+          allowNull: false,
+        },
+        created_at: {
+          type: Sequelize.DataTypes.DATE,
+          allowNull: true,
+        },
+        updated_at: {
+          type: Sequelize.DataTypes.DATE,
+          allowNull: true,
+        },
+      }
+    );
   },
 
-  down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+  down: async (queryInterface) => {
+   await queryInterface.dropTable('type');
   }
 };
