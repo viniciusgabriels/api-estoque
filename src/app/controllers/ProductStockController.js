4df937fe6a9b964183613b2f1e473e415f3443db
Martim-Fuhr <martimfuhr@hotmail.com>
2021-03-23 22:53:17 -0300

    commit martim

---
+++ src/app/controllers/ProductStockController.js
@@ -0,0 +1,43 @@
+import { Op } from 'sequelize';
+import ProductStock from '../models/ProductStock';
+
+class ProductStockController {
+  async index(request, response) {
+      return response.json(await ProductStock.findAll())
+  }
+
+  async show(request, response) {
+    const { id } = request.params;
+    return response.json(await ProductStock.findOne({ where: { id } }));
+  }
+
+  async store(request, response) {
+    const { quantity, product_id, stock_is } = request.body;
+
+    return response.json(await ProductStock.create({ quantity, product_id, stock_id }));
+  }
+
+
+  async update(request, response) {
+    const { id } = request.params;
+    const { quantity, product_id, stock_id } = request.body;
+    return response.json(
+      await ProductStock.create(
+        { quantity, product_id, stock_id },
+        {
+          where: { id },
+          returning: true,
+        }
+      )
+    );
+  }
+
+
+  async delete(request, response) {
+    const { id } = request.params;
+    await ProductStock.destroy({ where: { id } });
+    response.sendStatus(202);
+  }
+}
+
+export default new ProductStockController();
---
+++ src/app/models/ProductStock.js
@@ -0,0 +1,56 @@
+import Sequelize, { Model } from 'sequelize';
+
+class ProductStock extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        id: {
+          type: Sequelize.DataTypes.INTEGER,
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+        },
+        quantity: {
+          type: Sequelize.DataTypes.INTEGER,
+          allowNull: false,
+        },
+        stock_id: {
+          type: Sequelize.DataTypes.INTEGER,
+          allowNull: false,
+          references: {
+            model: 'stock',
+            key: 'id',
+          },
+        },
+		product_id: {
+          type: Sequelize.DataTypes.INTEGER,
+          allowNull: false,
+          references: {
+            model: 'product',
+            key: 'id',
+          },
+        },
+      },
+      {
+        sequelize,
+        tableName: 'product_stock'
+      }
+    );
+
+    return this;
+  }
+
+  static associate(models) {
+    this.belongsTo(models.Stock, {
+      as: 'stock',
+      foreignKey: 'stock_id',
+    });
+
+    this.belongsTo(models.Product, {
+    as: 'product',
+    foreignKey: 'product_id',
+    });
+  }
+}
+
+export default ProductStock;
