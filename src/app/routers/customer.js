2e67c95ae0728b4b3a19c456e52bc178030e3e09
Mateus Luciano <teeusdm@gmail.com>
2021-03-29 17:29:52 -0300

    add swagger documentation in route customer index

--- src/app/routers/customer.js
+++ src/app/routers/customer.js
@@ -4,7 +4,39 @@ import { validateData, validateCustomerExists } from '../middlewares/customer';
 
 const routes = new Router();
 
-routes.get('/customers', CustomerController.index);
+routes.get(
+  '/customers',
+  CustomerController.index
+  /*
+  #swagger.tags = ['Clientes']
+  #swagger.description = 'Rota para buscar todos os clientes'
+
+  #swagger.parameters['name'] = {
+    in: 'body',
+    description: 'Nome do cliente',
+    required: false,
+    type: 'string'
+  }
+  #swagger.parameters['phone'] = {
+    in: 'body',
+    description: 'Telefone do cliente',
+    required: false,
+    type: 'integer'
+  }
+  #swagger.response[200] = {
+    description: 'Clientes listados',
+    schema: {
+      minimum: 1
+      $ref: "#/definitions/customers"
+    }
+    responses: {
+      '200': {
+        description: "OK"
+      }
+    }
+  }
+*/
+);
 routes.get('/customers/:id', validateCustomerExists, CustomerController.show);
 routes.post('/customers', validateData, CustomerController.store);
 routes.put(
