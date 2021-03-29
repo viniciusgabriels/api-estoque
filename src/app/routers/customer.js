4ce945ab0650c0950c4a0d2124f75a1f551b5a75
Mateus Luciano <teeusdm@gmail.com>
2021-03-29 17:33:56 -0300

    add swagger documentation in route customer store

--- src/app/routers/customer.js
+++ src/app/routers/customer.js
@@ -11,23 +11,23 @@ routes.get(
   #swagger.tags = ['Clientes']
   #swagger.description = 'Rota para buscar todos os clientes'
 
-  #swagger.parameters['name'] = {
-    in: 'body',
-    description: 'Nome do cliente',
-    required: false,
-    type: 'string'
-  }
-  #swagger.parameters['phone'] = {
-    in: 'body',
-    description: 'Telefone do cliente',
-    required: false,
-    type: 'integer'
-  }
-  #swagger.response[200] = {
-    description: 'Clientes listados',
-    schema: {
-      minimum: 1
-      $ref: "#/definitions/customers"
+    #swagger.parameters['name'] = {
+      in: 'body',
+      description: 'Nome do cliente',
+      required: false,
+      type: 'string'
+    }
+    #swagger.parameters['phone'] = {
+      in: 'body',
+      description: 'Telefone do cliente',
+      required: false,
+      type: 'integer'
+    }
+    #swagger.response[200] = {
+      description: 'Clientes listados',
+      schema: {
+        minimum: 1
+        $ref: "#/definitions/customers"
     }
     responses: {
       '200': {
@@ -45,14 +45,54 @@ routes.get(
   #swagger.tags = ['Clientes']
   #swagger.description = 'Rota para procurar e listar um cliente pelo ID'
 
-  #swagger.parameters['id'] = {
-    in: 'path',
-    description: 'ID do cliente',
-    required: true,
-    type: 'integer'
+    #swagger.parameters['id'] = {
+      in: 'path',
+      description: 'ID do cliente',
+      required: true,
+      type: 'integer'
+    }
+    #swagger.response[200] = {
+      description: 'Cliente listado',
+      schema: {
+        minimum: 1
+        $ref: "#/definitions/customers"
+    }
+    responses: {
+      '200': {
+        description: "OK"
+      }
+    }
   }
-  #swagger.response[200] = {
-    description: 'Cliente listado',
+*/
+);
+routes.post(
+  '/customers',
+  validateData,
+  CustomerController.store
+  /*
+  #swagger.tags = ['Clientes']
+  #swagger.description = 'Rota para cadastrar um novo cliente'
+
+    #swagger.parameters['name'] = {
+      in: 'body',
+      description: 'Nome do cliente',
+      required: true,
+      type: 'string'
+    }
+    #swagger.parameters['phone'] = {
+      in: 'body',
+      description: 'Telefone do cliente',
+      required: true,
+      type: 'integer'
+    }
+    #swagger.parameters['regionId'] = {
+      in: 'body',
+      description: 'ID da região do cliente',
+      required: true,
+      type: 'integer'
+    }
+    #swagger.response[201] = {
+    description: 'Cliente cadastrado',
     schema: {
       minimum: 1
       $ref: "#/definitions/customers"
@@ -65,7 +105,6 @@ routes.get(
   }
 */
 );
-routes.post('/customers', validateData, CustomerController.store);
 routes.put(
   '/customers/:id',
   validateCustomerExists,
