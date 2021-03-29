ddd6be34eae2383317a354c27e16a5557b85fa72
Mateus Luciano <teeusdm@gmail.com>
2021-03-29 18:05:04 -0300

    add swagger documentation in route stock  index

--- src/app/routers/stock.js
+++ src/app/routers/stock.js
@@ -4,7 +4,26 @@ import { validateData, validateStockExists } from '../middlewares/stock';
 
 const routes = new Router();
 
-routes.get('/stock', StockController.index);
+routes.get(
+  '/stock',
+  StockController.index
+  /*
+    #swagger.tags = ['Estoque']
+    #swagger.description = 'Rota para buscar todos os estoques'
+
+    #swagger.parameters['local'] = {
+      in: 'body',
+      description: 'Local do estoque',
+      required: false,
+      type: 'string'
+    }
+
+    #swagger.response[200] = {
+      description: 'Estoques listados',
+      schema: { $ref: "#/definitions/stock" }
+    }
+  */
+);
 routes.get('/stock/:id', validateStockExists, StockController.show);
 routes.post('/stock', validateData, StockController.store);
 routes.put(
