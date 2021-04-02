57c6394c45848c4379917982c34065709c7eb3be
Mateus Luciano <teeusdm@gmail.com>
2021-04-02 07:50:42 -0300

    created swagger documentation nearby region index

--- src/app/routers/nearbyRegions.js
+++ src/app/routers/nearbyRegions.js
@@ -4,7 +4,25 @@ import { validateNumber, notSameNumber } from '../middlewares/nearby';
 
 const routes = new Router();
 
-routes.get('/nearbyregion', NearbyRegionController.index);
+routes.get('/nearbyregion', NearbyRegionController.index
+/*
+  #swagger.tags = ['Regiões próximas']
+  #swagger.description = 'Rota para listas todas as regiões próximas'
+
+   #swagger.parameters['region'] = {
+      in: 'body',
+      description: 'ID da região',
+      required: false,
+      type: 'integer'
+    }
+    #swagger.parameters['ID da região próxima'] = {
+      in: 'body',
+      description: 'Região próxima',
+      required: false,
+      type: 'integer'
+    }
+  */
+);
 routes.get('/nearbyregion/:id', NearbyRegionController.show);
 routes.post('/nearbyregion', [validateNumber, notSameNumber], NearbyRegionController.store);
 routes.put('/nearbyregion/:id', [validateNumber, notSameNumber], NearbyRegionController.update);
